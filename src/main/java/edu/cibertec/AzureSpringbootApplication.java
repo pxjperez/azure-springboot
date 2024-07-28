37c9108d0f1544b5ca0e150a4f00a38c79ec5e62
jperezgil <jperezgil@outlook.com>
2024-07-28 01:57:56 -0500

    first commit2

--- src/main/java/edu/cibertec/AzureSpringbootApplication.java
+++ src/main/java/edu/cibertec/AzureSpringbootApplication.java
@@ -1,4 +1,4 @@
-package edu.cibertec.azure_springboot;
+package edu.cibertec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/edu/cibertec/AzureSpringbootApplicationTests.java
+++ src/test/java/edu/cibertec/AzureSpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package edu.cibertec.azure_springboot;
+package edu.cibertec;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
