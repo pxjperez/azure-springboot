b92d2d48eb10fe9993007af277f9f6ea6f197cdc
jperezgil <jperezgil@outlook.com>
2024-07-28 01:49:37 -0500

    first commit

---
+++ src/main/java/edu/cibertec/azure_springboot/AzureSpringbootApplication.java
@@ -0,0 +1,13 @@
+package edu.cibertec.azure_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringbootApplication.class, args);
+	}
+
+}
